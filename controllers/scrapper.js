e1720fa31a229ca81f5d64337d6bf7c5e02a10a6
krlz <soy_solamente@hotmail.com>
2023-06-15 23:48:06 +0300

    add scrapper

--- controllers/scrapper.js
+++ controllers/scrapper.js
@@ -1,9 +1,11 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
-const sites = [
+let sites = [
     "https://pb.proxybay.cas"
 ];
+
+let notFoundSites = [];
 
 async function fetchProxyUrls() {
     const fetch = await import('node-fetch');
@@ -26,7 +28,7 @@ async function scrapeWebsite(site, searchValue) {
     const results = [];
     try {
         let success = false;
-        const browser = await puppeteer.launch({headless: "new"});
+        const browser = await puppeteer.launch({ headless: "new" });
         const page = await browser.newPage();
 
         const endpoints = [
@@ -35,7 +37,7 @@ async function scrapeWebsite(site, searchValue) {
         ];
 
         for (const endpoint of endpoints) {
-            await page.goto(endpoint, {timeout: 3000}); // Set timeout to 3 seconds
+            await page.goto(endpoint, { timeout: 3000 }); // Set timeout to 3 seconds
             const html = await page.content();
 
             const $ = cheerio.load(html);
@@ -52,7 +54,7 @@ async function scrapeWebsite(site, searchValue) {
                         const descriptionLink = parentList.find('a[href^="/description.php"]');
                         const descriptionText = descriptionLink.text().trim();
 
-                        results.push({magnet: href, description: descriptionText});
+                        results.push({ magnet: href, description: descriptionText });
                     }
                 });
                 return results;
@@ -61,6 +63,7 @@ async function scrapeWebsite(site, searchValue) {
 
         if (!success) {
             console.log(`${site} - No results found`);
+            notFoundSites.push(site);
             return results;
         }
 
@@ -68,14 +71,20 @@ async function scrapeWebsite(site, searchValue) {
         await browser.close();
     } catch (error) {
         console.error(`Error scraping ${site}:`, error.message);
+        notFoundSites.push(site);
         return results;
     }
+}
+
+function sortSites(sites) {
+    // Sort the sites based on previously found "not found" sites count
+    return sites.sort((a, b) => notFoundSites.filter(site => site === a).length - notFoundSites.filter(site => site === b).length);
 }
 
 async function scrapeWebsites(res, searchValue) {
     const fetchedSites = await fetchProxyUrls();
     const cleanedSites = fetchedSites.map((site) => site.replace(/\/$/, '')); // Remove trailing slashes
-    const allSites = [...sites, ...cleanedSites];
+    const allSites = sortSites([...sites, ...cleanedSites]); // Sort the sites
 
     for (const site of allSites) {
         const someResults = await scrapeWebsite(site, searchValue);
